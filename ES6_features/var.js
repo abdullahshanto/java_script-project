961db6560ad2c7b03700bb61371c66550d783f05
AbdullahShanto <abdurrahman7328@gmail.com>
2026-06-17 16:06:56 +0600

    var datatyoe problem

--- ES6_features/var.js
+++ ES6_features/var.js
@@ -1,5 +1,12 @@
-var age =18;
-if(age>=18){
-  var a= true;
+// var age =18;
+// if(age>=18){
+//   var a= true;
+// }
+// console.log(a);//but it should not access out of the block code,it makes no sense
+
+//but it would not works in function block code
+
+function hello(){
+  var username = "hello";
 }
-console.log(a);//but it should not access out of the block code,it makes no sense
+console.log(username);
