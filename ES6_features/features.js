a687d7e6561d336bfe22ec4a3c09915f14916abb
AbdullahShanto <abdurrahman7328@gmail.com>
2026-06-19 23:38:04 +0600

    update

--- ES6_features/features.js
+++ ES6_features/features.js
@@ -189,3 +189,26 @@
 // p1.greet();
 
 //  Makes OOP style easier than prototype system.
+
+
+
+
+
+
+
+
+
+
+
+// 16. Promises (async handling)
+// let promise = new Promise((resolve, reject) => {
+//   let success = true;
+
+//   if (success) resolve("Done");
+//   else reject("Error");
+// });
+
+// promise.then(res => console.log(res))
+//        .catch(err => console.log(err));
+
+//  Solves callback hell problem.
