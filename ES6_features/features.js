5d2f13c15ee235b01f5779f8bbcb116dab5cd316
AbdullahShanto <abdurrahman7328@gmail.com>
2026-06-24 23:22:24 +0600

    update

--- ES6_features/features.js
+++ ES6_features/features.js
@@ -236,13 +236,3 @@
 
 //  Prevents error if property doesn’t exist.
 
-
-
-
-
-// 19. Nullish Coalescing (??)
-// let value = null;
-
-// console.log(value ?? "default");
-
-//  Only replaces null or undefined (not 0 or "")
