b4ac0d73fba995e5899eae820934bcf42502d458
AbdullahShanto <abdurrahman7328@gmail.com>
2026-06-30 23:07:10 +0600

    updated

--- closure/closure.js
+++ closure/closure.js
@@ -120,20 +120,3 @@
 // //to check whether the data used or not 
 
 
-
-
-function cacheFunc() {
-  let cache = {};
-
-  return function (num) {
-    if (cache[num]) {
-      console.log("From cache");
-      return cache[num];
-    } else {
-      console.log("Calculating...");
-      let result = num * 2;
-      cache[num] = result;
-      return result;
-    }
-  };
-}
