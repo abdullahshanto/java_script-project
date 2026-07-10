fb4c7481045d214bcd85145daec532cdab0a6654
AbdullahShanto <abdurrahman7328@gmail.com>
2026-07-10 20:39:01 +0600

    updated

--- closure/closure.js
+++ closure/closure.js
@@ -100,6 +100,18 @@
 
 
 
+// function once() {
+//   let called = false;
+
+//   return function () {
+//     if (!called) {
+//       console.log("Running first time");
+//       called = true;
+//     } else {
+//       console.log("Already called");
+//     }
+//   };
+// }
 
 
 
